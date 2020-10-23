da421353e09c03a130451b33b52c68afdf1ef8f3
Caitlin Hemlock <hemlock_caitlin@berkeley.edu>
2020-10-22 17:17:10 -0700

    plots

--- src/4-immune-growth-pca.R
+++ src/4-immune-growth-pca.R
@@ -13,7 +13,7 @@ y1_exposure <- y1_exposure[,c(1, 4:18)]
 #remove observations with all missing data except childid
 y1_exposure <- y1_exposure[rowSums(is.na(y1_exposure)) != ncol(y1_exposure)-1,]
 nrow(y1_exposure) #remaining obs
-summary(sapply(y1_exposure, is.na)) #check missingness
+sum(is.na(y1_exposure)) #check missingness
 y1_id <- y1_exposure[,1] #store ids
 y1_exposure <- y1_exposure[,-1] #remove ids
 
@@ -33,13 +33,13 @@ y1_impute_scale <- scale(y1_impute, center = FALSE)
 #run pca
 y1_pca <- prcomp(y1_impute_scale)
 summary(y1_pca) #first PC accounts for 25.2% of variance
+screeplot(y1_pca, npcs = 10, type = "lines")
+#diagnostic plots
+screeplot(y1_pca, npcs = 10, type = "lines")
+biplot(y1_pca)
 
 #bind back to dataset
-<<<<<<< HEAD
 y1.pc.ids <- y1_pca$x[,c(1:10)]
-=======
-y1.pc.ids <- y1_pca$x 
->>>>>>> 4c641c4a9cf1f450ed2720455eec61abb0e48a26
 colnames(y1.pc.ids) <- paste(colnames(y1.pc.ids), "y1", sep = "_")
 y1.pc.ids <- cbind(childid = y1_id, y1.pc.ids)
 
@@ -74,7 +74,7 @@ y2_exposure <- y2_exposure[,c(1, 4:16)]
 #remove observations with all missing data except childid
 y2_exposure <- y2_exposure[rowSums(is.na(y2_exposure)) != ncol(y2_exposure)-1,]
 nrow(y2_exposure) #remaining obs
-summary(sapply(y2_exposure, is.na)) #check missingness
+sum(is.na(y2_exposure)) #check missingness #check missingness
 y2_id <- y2_exposure[,1] #store ids
 y2_exposure <- y2_exposure[,-1] #remove ids
 
@@ -94,13 +94,12 @@ y2_impute_scale <- scale(y2_impute, center = FALSE)
 #run pca
 y2_pca <- prcomp(y2_impute_scale)
 summary(y2_pca) #first PC accounts for 26.5% of variance
+#diagnostic plots
+screeplot(y2_pca, npcs = 10, type = "lines")
+biplot(y2_pca)
 
 #bind back to dataset
-<<<<<<< HEAD
 y2.pc.ids <- y2_pca$x[,c(1:10)] 
-=======
-y2.pc.ids <- y2_pca$x 
->>>>>>> 4c641c4a9cf1f450ed2720455eec61abb0e48a26
 colnames(y2.pc.ids) <- paste(colnames(y2.pc.ids), "y2", sep = "_")
 y2.pc.ids <- cbind(childid = y2_id, y2.pc.ids)
 
@@ -128,4 +127,30 @@ y2.loadings.plot <- ggplot(data = y2_loadings_long) +
   labs(legend = "Loading", y = "Cytokine", x = "Principal component")
 
 pca.results <- merge(y1.pc.ids, y2.pc.ids, by = "childid")
-write.csv(pca.results, file = "~/Documents/immune-growth/results/clustering pca/PCA results.csv")
+
+#density plots
+#y1
+long <- pivot_longer(as.data.frame(y1.pc.ids), cols = starts_with("PC"), 
+                     names_to = "PC", values_to = "value")
+
+long$PC <- factor(long$PC, levels = c("PC1_y1","PC2_y1","PC3_y1","PC4_y1","PC5_y1",
+                                      "PC6_y1","PC7_y1","PC8_y1","PC9_y1","PC10_y1"))
+
+y1.density.plots <- ggplot(data = long) +
+  geom_density(aes(x = value))+
+  facet_wrap(~PC)+
+  xlim(-2.5,2.5)
+
+#y2
+long <- pivot_longer(as.data.frame(y2.pc.ids), cols = starts_with("PC"), 
+                     names_to = "PC", values_to = "value")
+
+long$PC <- factor(long$PC, levels = c("PC1_y2","PC2_y2","PC3_y2","PC4_y2","PC5_y2",
+                                      "PC6_y2","PC7_y2","PC8_y2","PC9_y2","PC10_y2"))
+
+y2.density.plots <- ggplot(data = long) +
+  geom_density(aes(x = value))+
+  facet_wrap(~PC)+
+  xlim(-2.5,2.5)
+
+#write.csv(pca.results, file = "~/Documents/immune-growth/results/clustering pca/PCA results.csv")
